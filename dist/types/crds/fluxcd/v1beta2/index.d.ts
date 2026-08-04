db020538aa0ed8f7e439e3fb7abe318a0d64fd54
Marian Steinbach <marian@giantswarm.io>
2026-08-04 09:04:50 +0200

    fix(fluxcd): pin image CRDs to v1.1.0 to keep v1beta2 types (#72)

--- dist/types/crds/fluxcd/v1beta2/index.d.ts
+++ dist/types/crds/fluxcd/v1beta2/index.d.ts
@@ -4,4 +4,7 @@
 export * from './GitRepository';
 export * from './OCIRepository';
 export * from './HelmRepository';
+export * from './ImagePolicy';
+export * from './ImageRepository';
+export * from './ImageUpdateAutomation';
 //# sourceMappingURL=index.d.ts.map
